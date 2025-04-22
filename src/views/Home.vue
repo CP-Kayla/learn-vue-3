a3d3abb000407afcc715e18bac3de39a56e5c9c7
CP-Kayla <114318387+CP-Kayla@users.noreply.github.com>
2025-04-21 23:20:51 -0500

    Basic routing.

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Welcome to Vue Router Demo!</h1>
+        <p>Explore the navigation bar to see in action!</p>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
